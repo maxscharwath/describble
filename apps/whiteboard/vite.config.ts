0185f75949ec65270e5e289f0071b58c13ef8dab
Maxime Scharwath <maxscharwath@gmail.com>
2023-03-20 21:59:12 +0100

    fix: Add missing wasm example package

--- apps/whiteboard/vite.config.ts
+++ apps/whiteboard/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import wasm from 'vite-plugin-wasm'
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm()],
+  plugins: [react(), wasm(), topLevelAwait()],
 })
