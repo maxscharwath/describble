365f7a0c54502c6e168316a6d796bba32d093cda
Maxime Scharwath <maxscharwath@gmail.com>
2023-07-01 15:14:36 +0200

    refactor(vite.config.ts): remove unused global object from define property The global object within the define property was not being used, leading to unnecessary clutter in the code. Removing it simplifies the configuration and improves readability.

--- apps/whiteboard/vite.config.ts
+++ apps/whiteboard/vite.config.ts
@@ -7,9 +7,6 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [wasm(), topLevelAwait(), react()],
-	define: {
-		global: {},
-	},
 	resolve: {
 		alias: [
 			{find: '~core', replacement: fileURLToPath(new URL('./src/core', import.meta.url))},
