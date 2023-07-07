2c010439842b3d803be54277650bbf7d7e387472
Maxime Scharwath <maxscharwath@gmail.com>
2023-07-07 17:55:35 +0200

    feat(i18next.d.ts): add nsSeparator property to CustomTypeOptions interface The nsSeparator property was added to the CustomTypeOptions interface to allow for namespace separation in i18next. This change will enable more precise localization by allowing different sections of the application to have their own set of translations.

--- apps/whiteboard/src/@types/i18next.d.ts
+++ apps/whiteboard/src/@types/i18next.d.ts
@@ -2,6 +2,7 @@ import type translation from '../locales/en.json' assert {type: 'json'};
 
 declare module 'i18next' {
 	interface CustomTypeOptions {
+		nsSeparator: '.';
 		resources: typeof translation;
 	}
 }
